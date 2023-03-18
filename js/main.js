652af99760b7973e49919c8ae85b20fe7a3dc064
gizeli-pereira <gizeli.pereira1@gmail.com>
2023-03-18 15:49:21 -0500

    Add first variables

---
+++ js/main.js
@@ -0,0 +1,27 @@
+/*----- constants -----*/
+
+/*----- app's state (variables) -----*/
+let number = 0;
+let result = 0;
+let secretCodeColor = [];
+let colorSetArr = [];
+let setCounter = 0;
+let posCounter = 0;
+let winArr = [];
+let saveColor;
+let colorSet;
+
+
+
+/*----- cached element references -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+init();
+
+function init() {
+
+}
